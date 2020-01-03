ede30e13017c875ce320cbd1888b30e39bbee7b3
Carolyn Cole <cac9@princeton.edu>
2020-01-03 07:57:17 -0500

    Running through a list Also added instructions to the readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # meetup_cleanup_members
 Clean up members in a meetup group
+
+## Installation
+
+ 1. install geckodriver from `https://github.com/mozilla/geckodriver/releases`
+ 1. install ruby
+ 1. install bundler
+ 1. clone repo
+ 1. cd into the cloned directory and run `bundle install`
+
+
+ ## Running
+
+ 1. `EMAIL=<your meetup email>`
+ 1. `export EMAIL`
+ 1. `PASSWORD=<your meetup password>`
+ 1. `export PASSWORD`
+ 1. put ids in `ids.txt`
+ 1. `rspec`
+
--- spec/features/meetup_cleanup_spec.rb
+++ spec/features/meetup_cleanup_spec.rb
@@ -15,10 +15,13 @@ describe "the signin process", type: :feature, js: true do
     slow_typing(pass_word_field, ENV['PASSWORD'])
     click_button 'loginFormSubmit'
 
-    id = "4061909"
-    email_blurb = "To keep the Women's Adventure Club of Centre County affordable (Meet-Up charges based on # of members), we are reviewing our membership list and eliminating those members who have not visited the site in over a year. Please feel free to rejoin WAC if/when you have time to actively participate. Thank you and best wishes for a healthy and happy new year!"
-    url = "https://www.meetup.com/Womens-Adventure-Club-of-Centre-County/members/remove/?id=#{id}&emailBlurb=#{URI::encode(email_blurb)}&returnUri=https%3A%2F%2Fwww.meetup.com%2FWomens-Adventure-Club-of-Centre-County%2Fmembers%2F"
-    visit url
-    click_button 'Remove member'
+    File.open('ids.txt').each do |id|
+      puts id
+      email_blurb = "To keep the Women's Adventure Club of Centre County affordable (Meet-Up charges based on # of members), we are reviewing our membership list and eliminating those members who have not visited the site in over a year. Please feel free to rejoin WAC if/when you have time to actively participate. Thank you and best wishes for a healthy and happy new year!"
+      url = "https://www.meetup.com/Womens-Adventure-Club-of-Centre-County/members/remove/?id=#{id}&emailBlurb=#{URI::encode(email_blurb)}&returnUri=https%3A%2F%2Fwww.meetup.com%2FWomens-Adventure-Club-of-Centre-County%2Fmembers%2F"
+      visit url
+      click_button 'Remove member'
+      sleep(10)
+    end
   end
 end
